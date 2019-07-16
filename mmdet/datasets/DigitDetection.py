ab85af4914952cd2336ae76b3b40d2a5e6f50abd
yijie <zhangyj@kilox.cn>
2019-07-16 15:48:07 +0800

    add more

--- mmdet/datasets/DigitDetection.py
+++ mmdet/datasets/DigitDetection.py
@@ -1,3 +1,8 @@
-from .voc import VOCDataset
-class DigitDetection(VOCDataset):
+from .registry import DATASETS
+from .xml_style import XMLDataset
+
+@DATASETS.register_module
+class DigitDetection(XMLDataset):
 	CLASSES = ('0','1','2','3','4','5','6','7','8','9')
+	def __init__(self, **kwargs):
+		super(DigitDetection, self).__init__(**kwargs)
--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -7,6 +7,7 @@ from .loader import GroupSampler, DistributedGroupSampler, build_dataloader
 from .utils import to_tensor, random_scale, show_ann
 from .dataset_wrappers import ConcatDataset, RepeatDataset
 from .FaultDetetcion import FaultDetection
+from .DigitDetection import DigitDetection
 from .extra_aug import ExtraAugmentation
 from .registry import DATASETS
 from .builder import build_dataset
@@ -15,5 +16,5 @@ __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset', 'GroupSampler',
     'DistributedGroupSampler', 'build_dataloader', 'to_tensor', 'random_scale',
     'show_ann', 'ConcatDataset', 'RepeatDataset', 'ExtraAugmentation',
-    'WIDERFaceDataset', 'DATASETS', 'build_dataset','FaultDetection'
+    'WIDERFaceDataset', 'DATASETS', 'build_dataset','FaultDetection',DigitDetection
 ]
